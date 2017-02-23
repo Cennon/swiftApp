db464f0a1695f244b1968f88e044ca70ca348896
Christopher Cennon <Christopher@Fozzie.local>
2017-02-22 22:15:45 -0500

    My first commit

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Christopher Cennon on 2017-02-22.
+//  Copyright © 2017 Christopher Cennon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount >= 10 {
+            theLabel.text = "BUTTONPUSHER"
+        }
+        
+    }
+    
+    @IBAction func button2Tapped(_ sender: Any) {
+        
+        theLabel.text  = "Buttons are cool!"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
